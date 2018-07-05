cbd18d3e28fb65ea69676486bb3e3c77cfa56acf
Abdu Maadjid <abdumaadjidcandi@gmail.com>
2018-07-05 23:00:51 +0700

    Revisi Database

--- others/DataBase/fladeodb.sql
+++ others/DataBase/fladeodb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 01, 2018 at 03:28 PM
+-- Generation Time: Jul 05, 2018 at 05:58 PM
 -- Server version: 10.1.32-MariaDB
 -- PHP Version: 5.6.36
 
@@ -29,24 +29,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `barang` (
-  `Art_Barang` varchar(20) NOT NULL,
+  `Id_Barang` int(11) NOT NULL,
+  `Kd_Barang` int(8) NOT NULL,
   `Jenis_Barang` varchar(20) NOT NULL,
   `Hrg_Beli` double NOT NULL,
   `Hrg_Jual` double NOT NULL,
   `Kd_Supplier` varchar(7) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `barang_detail`
---
-
-CREATE TABLE `barang_detail` (
-  `Id` int(11) NOT NULL,
-  `Art_Barang` varchar(20) NOT NULL,
-  `Size` varchar(20) NOT NULL,
-  `Qty_Barang` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -58,6 +46,48 @@ CREATE TABLE `barang_detail` (
 CREATE TABLE `data_akun` (
   `Kd_Akun` varchar(8) NOT NULL,
   `Nm_Akun` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_jurnal`
+--
+
+CREATE TABLE `detail_jurnal` (
+  `Id` int(11) NOT NULL,
+  `No_Jurnal` varchar(10) NOT NULL,
+  `Kd_Akun` varchar(8) NOT NULL,
+  `Debet` double NOT NULL,
+  `Kredit` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_pelunasan_pembayaran`
+--
+
+CREATE TABLE `detail_pelunasan_pembayaran` (
+  `Id` int(11) NOT NULL,
+  `No_Pembayaran` varchar(7) NOT NULL,
+  `No_Transaksi` varchar(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_purchase_order`
+--
+
+CREATE TABLE `detail_purchase_order` (
+  `Id` int(11) NOT NULL,
+  `No_PO` varchar(6) NOT NULL,
+  `Art_Barang` varchar(20) NOT NULL,
+  `Size` varchar(20) NOT NULL,
+  `Qty_Order` int(11) NOT NULL,
+  `Harga_Beli` double NOT NULL,
+  `Harga_Jual` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -76,20 +106,6 @@ CREATE TABLE `jurnal` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `jurnal_detail`
---
-
-CREATE TABLE `jurnal_detail` (
-  `Id` int(11) NOT NULL,
-  `No_Jurnal` varchar(10) NOT NULL,
-  `Kd_Akun` varchar(8) NOT NULL,
-  `Debet` double NOT NULL,
-  `Kredit` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `pelunasan_pembayaran`
 --
 
@@ -97,18 +113,6 @@ CREATE TABLE `pelunasan_pembayaran` (
   `No_Pembayaran` varchar(7) NOT NULL,
   `Jth_Tempo` date NOT NULL,
   `Total_Bayar` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `pelunasan_pembayaran_detail`
---
-
-CREATE TABLE `pelunasan_pembayaran_detail` (
-  `Id` int(11) NOT NULL,
-  `No_Pembayaran` varchar(7) NOT NULL,
-  `No_Transaksi` varchar(9) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -135,22 +139,6 @@ CREATE TABLE `purchase_order` (
   `Tgl_Kirim` date NOT NULL,
   `Kd_Supplier` varchar(7) NOT NULL,
   `Total_Qty` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `purchase_order_detail`
---
-
-CREATE TABLE `purchase_order_detail` (
-  `Id` int(11) NOT NULL,
-  `No_PO` varchar(6) NOT NULL,
-  `Art_Barang` varchar(20) NOT NULL,
-  `Size` varchar(20) NOT NULL,
-  `Qty_Order` int(11) NOT NULL,
-  `Harga_Beli` double NOT NULL,
-  `Harga_Jual` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -187,12 +175,25 @@ CREATE TABLE `transaksi_pembelian` (
 --
 
 CREATE TABLE `user` (
-  `Id` int(11) NOT NULL,
+  `Id_User` int(11) NOT NULL,
   `Username` varchar(8) NOT NULL,
   `Nama` varchar(50) NOT NULL,
   `Hak_Akses` varchar(10) NOT NULL,
   `Password` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`Id_User`, `Username`, `Nama`, `Hak_Akses`, `Password`) VALUES
+(9, 'Fnc-001', 'Player 3', 'Finance', '1234'),
+(10, 'Fnc-002', 'Player 3', 'Finance', '1234'),
+(11, 'Pch-001', 'Player 3', 'Purchasing', '1234'),
+(12, 'Pch-002', 'Player 3', 'Purchasing', '1234'),
+(13, 'Pch-003', 'Player 3', 'Purchasing', '1234'),
+(14, 'Fnc-003', 'Player 3', 'Finance', '1234'),
+(15, 'Fnc-004', 'Player 3', 'Finance', '1234');
 
 --
 -- Indexes for dumped tables
@@ -202,13 +203,7 @@ CREATE TABLE `user` (
 -- Indexes for table `barang`
 --
 ALTER TABLE `barang`
-  ADD PRIMARY KEY (`Art_Barang`);
-
---
--- Indexes for table `barang_detail`
---
-ALTER TABLE `barang_detail`
-  ADD PRIMARY KEY (`Id`);
+  ADD PRIMARY KEY (`Id_Barang`);
 
 --
 -- Indexes for table `data_akun`
@@ -217,28 +212,34 @@ ALTER TABLE `data_akun`
   ADD PRIMARY KEY (`Kd_Akun`);
 
 --
+-- Indexes for table `detail_jurnal`
+--
+ALTER TABLE `detail_jurnal`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `detail_pelunasan_pembayaran`
+--
+ALTER TABLE `detail_pelunasan_pembayaran`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `detail_purchase_order`
+--
+ALTER TABLE `detail_purchase_order`
+  ADD PRIMARY KEY (`Id`);
+
+--
 -- Indexes for table `jurnal`
 --
 ALTER TABLE `jurnal`
   ADD PRIMARY KEY (`No_Jurnal`);
 
 --
--- Indexes for table `jurnal_detail`
---
-ALTER TABLE `jurnal_detail`
-  ADD PRIMARY KEY (`Id`);
-
---
 -- Indexes for table `pelunasan_pembayaran`
 --
 ALTER TABLE `pelunasan_pembayaran`
   ADD PRIMARY KEY (`No_Pembayaran`);
-
---
--- Indexes for table `pelunasan_pembayaran_detail`
---
-ALTER TABLE `pelunasan_pembayaran_detail`
-  ADD PRIMARY KEY (`Id`);
 
 --
 -- Indexes for table `penerimaan_barang`
@@ -251,12 +252,6 @@ ALTER TABLE `penerimaan_barang`
 --
 ALTER TABLE `purchase_order`
   ADD PRIMARY KEY (`No_PO`);
-
---
--- Indexes for table `purchase_order_detail`
---
-ALTER TABLE `purchase_order_detail`
-  ADD PRIMARY KEY (`Id`);
 
 --
 -- Indexes for table `supplier`
@@ -274,7 +269,7 @@ ALTER TABLE `transaksi_pembelian`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`Id`),
+  ADD PRIMARY KEY (`Id_User`),
   ADD UNIQUE KEY `Username` (`Username`);
 
 --
@@ -282,34 +277,34 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT for table `barang_detail`
+-- AUTO_INCREMENT for table `barang`
 --
-ALTER TABLE `barang_detail`
+ALTER TABLE `barang`
+  MODIFY `Id_Barang` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `detail_jurnal`
+--
+ALTER TABLE `detail_jurnal`
   MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `jurnal_detail`
+-- AUTO_INCREMENT for table `detail_pelunasan_pembayaran`
 --
-ALTER TABLE `jurnal_detail`
+ALTER TABLE `detail_pelunasan_pembayaran`
   MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `pelunasan_pembayaran_detail`
+-- AUTO_INCREMENT for table `detail_purchase_order`
 --
-ALTER TABLE `pelunasan_pembayaran_detail`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `purchase_order_detail`
---
-ALTER TABLE `purchase_order_detail`
+ALTER TABLE `detail_purchase_order`
   MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `Id_User` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
